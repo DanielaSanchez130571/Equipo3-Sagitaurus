30c2ddc7dbe01cb25fe537c9b47579fdf383bb9e
DanHT22 <tolumes.daniela@gmail.com>
2023-06-16 01:07:57 -0600

    ajustes

--- JS/carrito-compra.js
+++ JS/carrito-compra.js
@@ -1,17 +1,20 @@
 function guardar_datos(event){
     event.preventDefault()
-    // console.log("funciona")
-    const $USERNAME = document.querySelector("#nombre-usuario").value
 
+    const $USERNAME = document.querySelector("#nombre-usuario").value
+    const $APELLIDO = document.querySelector("#apellido-usuario").value
 
     console.log("Nombre: ",$USERNAME)
+    console.log("Apellido: ",$APELLIDO)
+   
+
     
 
     /* mostrar una alerta cuando se envia formulario */
     alert("Formulario enviado correctamente")
 
     /* redirigir a una pagina nueva */
-    window.location.href = "pagina2.html"
+    window.location.href = "../index.html"
 }
 
 const $FORM = document.getElementById ("formulario__form")
